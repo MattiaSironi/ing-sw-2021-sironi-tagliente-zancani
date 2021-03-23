0029339eaa7d85d6ce67bc2e54fbf61733ab5169
smntgl <simone.tagliente@mail.polimi.it>
2021-03-23 23:14:42 +0100

    Second commit of Simone Tagliente (Manca il metodo che gestisce le risorse all'interno del warehouse)

---
+++ src/main/java/it/polimi/ingsw/model/LorenzoIlMagnifico.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model;
+
+public class LorenzoIlMagnifico {
+    int blackCrossPos;
+
+    public LorenzoIlMagnifico(int blackCrossPos) {
+        this.blackCrossPos = blackCrossPos;
+    }
+
+    public int getBlackCrossPos() {
+        return blackCrossPos;
+    }
+
+    public void setBlackCrossPos(int blackCrossPos) {
+        this.blackCrossPos = blackCrossPos;
+    }
+}
