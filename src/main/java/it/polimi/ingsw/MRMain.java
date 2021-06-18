56a2dc1d7c87783aa4ac5f28ce46352bc7bb9821
GIGIProgrammer <mattia1.sironi@mail.polimi.it>
2021-06-18 15:41:41 +0200

    feat : you can choose what main run using args.

--- src/main/java/it/polimi/ingsw/MRMain.java
+++ src/main/java/it/polimi/ingsw/MRMain.java
@@ -10,12 +10,21 @@ import java.util.Scanner;
 public class MRMain {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Welcome to Master of Renaissance\n\n0 -> Run the server\n1 -> Run CLI (command line interface)\n2 -> Run GUI (graphics user interface)");
-        Scanner in = new Scanner(System.in);
-        switch(in.nextLine()){
-            case "0" -> Server.main(null);
-            case "1" -> CLI.main(null);
-            default -> GUI.main(null);
+        if (args.length ==0) {
+            System.out.println("Welcome to Master of Renaissance\n\n0 -> Run the server\n1 -> Run CLI (command line interface)\n2 -> Run GUI (graphics user interface)");
+            Scanner in = new Scanner(System.in);
+            switch (in.nextLine()) {
+                case "0" -> Server.main(null);
+                case "1" -> CLI.main(null);
+                default -> GUI.main(null);
+            }
+        } else {
+            switch (args[0]) {
+                case "server" -> Server.main(null);
+                case "CLI" -> CLI.main(null);
+                case "GUI" -> GUI.main(null);
+                default -> System.exit(0);
+            }
         }
     }
 }
