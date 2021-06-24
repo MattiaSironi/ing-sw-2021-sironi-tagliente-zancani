8b8d7ab4126fdc733ca82f60ebacdf850fe383d4
GIGIProgrammer <mattia1.sironi@mail.polimi.it>
2021-06-24 22:19:41 +0200

    add: JavaDoc CLI

--- src/main/java/it/polimi/ingsw/client/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/client/cli/CLI.java
@@ -11,6 +11,10 @@ import it.polimi.ingsw.model.Game;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Class CLI is the Class that interfaces with user. it prints to console and it scan user inputs.
+ */
+
 public class CLI {
     private final Scanner in = new Scanner(System.in);
 
@@ -31,6 +35,11 @@ public class CLI {
 
     }
 
+    /**
+     * Method startMultiPlayer creates the socket, the View part and the Controller part of Client
+     *
+     */
+
 
     public static void startMultiPlayer() throws IOException {
         CLI cli = new CLI();
@@ -40,6 +49,10 @@ public class CLI {
         ssc.setUI(cms);
         cms.setupMultiplayer();
     }
+
+    /**
+     * Method startSinglePlayer creates a Game in single player mode, recreating the MVC pattern Client-side.
+     */
 
 
     public static void startSinglePlayer() {
@@ -55,14 +68,30 @@ public class CLI {
 
     }
 
+    /**
+     * Method readFromInput reads user's input commands.
+     * @return an input command,
+     */
+
     public synchronized String readFromInput() {
         System.out.print("> ");
         return in.nextLine();
     }
 
+    /**
+     * Method printToConsole prints a message to console.
+     * @param message String to print.
+     */
+
     public synchronized void printToConsole(String message) {
         System.out.println(message);
     }
+
+    /**
+     * Method printErrorToConsole prints a message in red. it's used for errors found during parsing in ClientActionController's methods.
+     * @see ClientActionController
+     * @param message String to print.
+     */
 
     public synchronized void printErrorToConsole(String message) {
         System.out.println(Color.ANSI_RED + message + Color.ANSI_RESET);
